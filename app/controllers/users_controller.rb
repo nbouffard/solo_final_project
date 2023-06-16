7f723df881ba30866c22021b355a4fb0dfd3c48f
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-06-16 12:34:40 +0200

    add update, add bio to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,21 @@ class UsersController < ApplicationController
   def edit
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit, status: :unproccessable_entity
+    end
+  end
+
   private
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:bio)
   end
 end
