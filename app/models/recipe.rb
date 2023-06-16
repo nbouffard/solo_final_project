6784e30e5b2bab27b1d9739eabcb278894aaa7fb
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-06-16 10:43:24 +0200

    add recipe associations and validates

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :events
+  validates :title, :description, :ingredients, :category, :cuisine,
+            :prep_time, :total_time, :difficulty, :servings, presence: true
 end
