08c71a03e0195424a67081652c9d5d651afb46b9
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-06-16 10:39:39 +0200

    add user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :recipes
+  has_many :reviews
+  has_many :bookings
+  has_many :events
+
+  validates :nickname, :first_name, :last_name, :address, :date_of_birth, presence: true
+
 end
