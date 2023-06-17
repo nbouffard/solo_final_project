1989a060df92a6a34af2eb49a410c9a997afa600
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-06-17 09:51:47 +0200

    add policy///////'

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -4,8 +4,8 @@ class ApplicationPolicy
   attr_reader :user, :record
 
   def initialize(user, record)
-    @user = user
-    @record = record
+    @user = user # current_user
+    @record = record # the instance that is being checked ie restaurant or review etc
   end
 
   def index?
